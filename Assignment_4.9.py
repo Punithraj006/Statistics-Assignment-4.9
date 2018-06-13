5fd4962ab5bb1149176ad77413cf065803a6f335
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 12:02:55 +0530

    Statistics Assignment 4.9

---
+++ Assignment_4.9.py
@@ -0,0 +1,17 @@
+Problem Statement: 4.9
+
+You take the SAT and score 1100. The mean score for the SAT is 1026 and the standard
+deviation is 209. How well did you score on the test compared to the average test taker?
+
+Solution:
+    
+    Here X value = 1100 
+    
+    Z-score = 1100 - mean/Standard deviation
+    
+    Z = 1100-1026/209 = 0.354
+    
+   Find the z-value in the z-table :
+     A z-score of 0.354 = 0.1368 + .5000* = .6368 or 63.68%.
+     
+     [Note: The z-table shown has scores for the right of the mean. Therefore, we have to add .5000]
